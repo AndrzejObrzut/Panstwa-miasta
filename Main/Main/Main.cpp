5c0793b46bd8894b806773eed670fd1cfd9d6fbf
PiotrPawlus2 <pawlus.piotrekdev@gmail.com>
2016-05-11 15:17:08 +0100

    Add SFML files, test SFML graphics by render a circle

--- Main/Main/Main.cpp
+++ Main/Main/Main.cpp
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <SFML/Graphics.hpp>
+
 
 using namespace std;
 
@@ -15,13 +17,29 @@ int main()
 	srand(time(NULL));
 
 
-	Menu menu;
-	bool option = menu.runMenu();
-	if (option == false) {
-		return 0;
-	}
-	else {
-		menu.runMenu();
+	//Menu menu;
+	//bool option = menu.runMenu();
+	//if (option == false) {
+	//	return 0;
+	//}
+	//else {
+	//	menu.runMenu();
+	//}
+
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		window.clear();
+		window.draw(shape);
+		window.display();
 	}
 
 	system("pause");
