ccda53071d0552bbb770e00abeee3e480ca3f3db
PiotrPawlus2 <pawlus.piotrekdev@gmail.com>
2016-05-13 17:13:08 +0100

    Add menu text(label)

--- Main/Main/Main.cpp
+++ Main/Main/Main.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 int main()
 {
-	srand(time(NULL));
+	//srand(time(NULL));
 
 
 	//Menu menu;
@@ -27,6 +27,7 @@ int main()
 	//	menu.runMenu();
 	//}
 
+	// WINDOW OF GAME
 	int windowWidth = 1024;
 	int windowHeight = 768;
 	sf::String title("Panstwa Miasta");
@@ -34,6 +35,20 @@ int main()
 	sf::RenderWindow window(size, title);
 	window.clear(sf::Color(56, 134, 185, 255));
 
+
+	// MENU CONTROLLER
+	sf::Font font;
+	if (!font.loadFromFile("fonts/arial.ttf")) { cout << "Problem with loading fonts"; }
+
+	const float MENU_LABEL_SCREEN_POS_X = 0.50;
+	const float MENU_LABEL_SCREEN_POS_Y = 0.30;
+
+	sf::Text menuLabel("Panstwa Miasta", font);
+	menuLabel.setCharacterSize(60);
+	menuLabel.setColor(sf::Color::White);
+	float menuLabelPosX = windowWidth * MENU_LABEL_SCREEN_POS_X - menuLabel.getGlobalBounds().width * 0.5;
+	float menuLabelPosY = windowHeight * MENU_LABEL_SCREEN_POS_Y - menuLabel.getGlobalBounds().height * 0.5;
+	menuLabel.setPosition(sf::Vector2f(menuLabelPosX, menuLabelPosY));
 
 	sf::String singlePlayerButtonPath("image/SinglePlayer.png");
 	sf::String singlePlayerButtonHoverPath("image/SinglePlayerButtonShadow.png");
@@ -46,11 +61,10 @@ int main()
 	float singlePlayerButtonPosY = windowHeight * SINGLE_BUTTON_SCREEN_POS_Y - singleButtonHeight * 0.5;
 	singlePlayerButton.setPosition(sf::Vector2f(singlePlayerButtonPosX, singlePlayerButtonPosY));
 
-	sf::RectangleShape ttBox;
-	ttBox.setSize(sf::Vector2f(120, 120));
-	ttBox.setFillColor(sf::Color(0, 0, 0, 175));
-	sf::IntRect ttBoxRect(ttBox.getPosition().x, ttBox.getPosition().y, ttBox.getSize().x, ttBox.getSize().y);
+	// SET SINGLE GAME
 
+
+	// HANDLING ACTIONS
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
@@ -72,12 +86,15 @@ int main()
 		}
 
 		window.clear(sf::Color(56, 134, 185, 255));
-		if (!singlePlayerButton.isHidden()) { window.draw(singlePlayerButton); }
-		
+		if (!singlePlayerButton.isHidden()) { 
+			window.draw(singlePlayerButton); 
+			window.draw(menuLabel);
+		}
+
 		window.display();
 	}
 
-	system("pause");
+	//system("pause");
     return 0;
 }
 
